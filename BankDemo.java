e0e0fb54a778b6360d9194516dbe68fcc50b0f8e
Gr21314 <37418166+Gr21314@users.noreply.github.com>
2020-10-05 11:09:45 -0400

    Update BankDemo.java

--- BankDemo.java
+++ BankDemo.java
@@ -1,10 +1,17 @@
 // Import Scanner Class
-
+import java.util.Scanner;
 public class BankDemo {
 
     public static void main(String[] args) {
         // Initialize Scanner Object
+        Scanner input = new Scanner(System.in);
         // Initialize BankAccount Object
+        BankAccount john = new BankAccount(200.00);
+        System.out.println(john.getBalance());
+        john.setBalance(5000.00);
+        System.out.println(john.getBalance());
+
+        // System.out.println("Please enter beginning balance: ")
 
     }
 }
