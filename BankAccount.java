5e6340a98f27fdd86d3fd7a615e1e8d0f5217eee
Gr21314 <37418166+Gr21314@users.noreply.github.com>
2020-10-05 11:08:54 -0400

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,22 @@
 public class BankAccount {
 
     // Instance Variables
+    private double balance; // keeps track of the amount of money a person has
 
     // Constructors
+    public BankAccount(){ // Defult Constructor
+        this.balance = 0.00;
+    }
+    public BankAccount(double pBalance){
+        this.balance = pBalance;
+    }
 
     // Methods
+    public void setBalance(double pBalance){
+        this.balance = pBalance;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
 
 }
